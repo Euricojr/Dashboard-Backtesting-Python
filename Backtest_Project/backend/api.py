ae143cf91a3e305fc597164152a380d597ef5978
EuricoJr <ddom1459@gmail.com>
2026-01-25 14:21:58 -0300

    Inclusão landing page no backtest project

--- Backtest_Project/backend/api.py
+++ Backtest_Project/backend/api.py
@@ -17,6 +17,10 @@ app = Flask(__name__, static_folder='../frontend', static_url_path='')
 
 @app.route('/')
 def home():
+    return app.send_static_file('landing.html')
+
+@app.route('/dashboard')
+def dashboard():
     return app.send_static_file('index.html')
 CORS(app)
 
